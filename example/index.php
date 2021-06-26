05c9c89683a0eba60d5c0879e0198f562944774d
ekharisma <65415399+ekharisma@users.noreply.github.com>
2021-06-27 02:55:49 +0700

    Update index.php
    Delete some residual benchmark

--- example/index.php
+++ example/index.php
@@ -1,13 +1,9 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
-$startTime = microtime(true);
 $curl = new \Yuana\Curl();
 for ($i=0; $i < 10; $i++) { 
     $response = $curl->get('http://belanja-api.herokuapp.com', ["users_id" => 2]);
 }
-$finishTime = microtime(true);
 
-echo "Time elapsed: ". $response;
-echo "<br>";  
-echo $finishTime - $startTime;
+echo $response;
